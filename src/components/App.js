54f02230e28a1176474c09342e801970445e201a
Bryan Num <bnum33@gmail.com>
2022-05-11 12:19:23 -0700

    basic prompt form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
-import ".style/App.css";
+import "../style/App.css";
+import PromptForm from "./PromptForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>Welcome to GPT-3 Fun</div>
+      <PromptForm />
+    </div>
+  );
 }
 
 export default App;
